014edc2d3119ead2fee1e29c88afa24d1f3ce4ef
wahyudwiprstka <wahyudwiprstka@gmail.com>
2024-05-03 12:33:15 +0800

    changesss

--- app/store/add/page.jsx
+++ app/store/add/page.jsx
@@ -23,6 +23,7 @@ const AddStore = () => {
   useEffect(() => {
     if(status == 'unauthenticated'){
       setIsLoaded(false);
+      router.push('/unauthorized');
     }else if(status == 'authenticated'){
       setIsLoaded(true);
     }
@@ -62,29 +63,6 @@ const AddStore = () => {
       {!isLoaded ? <span className="loading loading-spinner loading-lg"></span> :
     <div className="min-h-[100vh] flex flex-col items-center">
       <div className="w-[50rem]">
-        {/* {(loginerr || error) && (
-          <div
-            role="alert"
-            className="alert alert-error bg-red-500 text-white w-[20vw] mx-auto mb-4 shadow "
-          >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="stroke-current shrink-0 h-6 w-6"
-              fill="none"
-              viewBox="0 0 24 24"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"
-              />
-            </svg>
-            {loginerr ? <span>{loginerr}</span> : <span>{error}</span>} */}
-        {/* <span>{error}</span> */}
-        {/* </div> */}
-        {/* )} */}
-
         {/* Error */}
         <div className="py-10">
           <div
